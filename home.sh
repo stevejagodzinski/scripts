bb4ca7e2d0e1dcb5b007c00da1643defc33dcf26
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2020-02-28 02:32:21 -0500

    Only turn off proxy if proxy is on

--- home.sh
+++ home.sh
@@ -5,7 +5,10 @@ echo "Switched to Home location."
 
 while true
 do
-  networksetup -setautoproxystate "Wi-Fi" off
-  echo "Turned off proxy on 'Wi-Fi' adapter."
+  isProxyEnabled=`networksetup -getautoproxyurl "Wi-Fi" | tail -c -3`
+  if [ "$isProxyEnabled" != "No" ]; then
+    networksetup -setautoproxystate "Wi-Fi" off
+    echo "Turned off proxy on 'Wi-Fi' adapter."
+  fi
   sleep 30
 done
