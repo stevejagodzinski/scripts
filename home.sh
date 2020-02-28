e712d57433e7857cbf6704aba2b0abd4fc731cfa
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2020-02-28 03:01:40 -0500

    Make refresh rate a variable
    Print that script is refreshing to indicate that things are happening

--- home.sh
+++ home.sh
@@ -2,6 +2,7 @@
 
 NETWORK_ADAPTER="Wi-Fi"
 HOME_LOCATION="Home"
+REFRESH_RATE_SECONDS=30
 
 echo "Looking for network adapter named: $NETWORK_ADAPTER"
 
@@ -9,13 +10,16 @@ echo "Switching to '$HOME_LOCATION' location"
 networksetup -switchtolocation $HOME_LOCATION
 echo "\nSwitched to '$HOME_LOCATION' location"
 
-echo "Turning proxy off on '$NETWORK_ADAPTER' adapter"
+echo "Turning proxy off on '$NETWORK_ADAPTER' adapter."
+echo "Refreshing every $REFRESH_RATE_SECONDS seconds..."
+
 while true
 do
+  echo "enhance..."
   isProxyEnabled=`networksetup -getautoproxyurl "$NETWORK_ADAPTER" | tail -c -3`
   if [ "$isProxyEnabled" != "No" ]; then
     networksetup -setautoproxystate "Wi-Fi" off
     echo "Turned off proxy on '$NETWORK_ADAPTER' adapter"
   fi
-  sleep 30
+  sleep $REFRESH_RATE_SECONDS
 done
